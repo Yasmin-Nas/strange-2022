f47f404ad004ddd672b640391b91cfbd931932a3
YASMIN SANTOS NASCIMENTO <22102428@CFP127ALUNO.LOCAL>
2022-04-12 11:30:30 -0300

    aula 12/04

---
+++ js/JS main.js
@@ -0,0 +1,6 @@
+const menu= document.querySelector('.menu-burger-container')
+
+menu.addEventListener('click', menuAction)
+    menu.classList.toggle ('show')
+
+menu.addEventListener('click', menuAction)
